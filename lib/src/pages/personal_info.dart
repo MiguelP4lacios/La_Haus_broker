7854325f2c89b3818e89524aa8e3321b97dbc596
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-10-16 08:37:30 -0500

    Creating structure of personal info

--- lib/src/pages/personal_info.dart
+++ lib/src/pages/personal_info.dart
@@ -8,8 +8,50 @@ class PersonalPage extends StatefulWidget {
 class _PersonalPageState extends State<PersonalPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      // appBar: AppBar(
+      //   backgroundColor: Colors.white,
+      //   leading: GestureDetector(
+      //     child: IconButton(
+      //       iconSize: 50.0,
+      //       color: Color.fromRGBO(103, 103, 103, 100),
+      //       icon: new Icon(Icons.keyboard_arrow_left),
+      //       onPressed: () {
+      //         Navigator.of(context).pushNamed('profile');
+      //       },
+      //     ),
+      //   ),
+      //   title: const Text('Información personal'),
+      // ),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            // Botones superiores (puede ser un appbar)
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                iconSize: 50.0,
+                color: Color.fromRGBO(103, 103, 103, 100),
+                icon: new Icon(Icons.keyboard_arrow_left),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('profile');
+                },
+              ),
+              Text(
+                'Información personal',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Color.fromRGBO(103, 103, 103, 100),
+                    fontWeight: FontWeight.w400),
+              ), // fontFamily: 'Raleway',
+            ],
+          ),
+          SizedBox(height: 100), // espacio
+        ],
+      ),
     );
   }
 }
--- lib/src/pages/profile.dart
+++ lib/src/pages/profile.dart
@@ -1,22 +1,18 @@
-
 import 'package:flutter/material.dart';
 
 class Perfil extends StatefulWidget {
-
-
   @override
   _PerfilState createState() => _PerfilState();
 }
 
 // ignore: unused_element
 class _PerfilState extends State<Perfil> {
-
   int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
-  setState(() {
-    _selectedIndex = index;
-  });
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
@@ -24,110 +20,128 @@ class _PerfilState extends State<Perfil> {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            SizedBox(height: 10),
-            Card( // Rectangulo superior
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-              margin: const EdgeInsets.all(8.0),
-              color: Color.fromRGBO(0, 208, 175, 100),
-              child: ListTile(
-                onTap: () {
-                  // function of profile
-                  
-                },
-                title: Text("Miguel Palacios", style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w900)),
-                subtitle: Text('Medellin',  style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500)),
-                leading: CircleAvatar(
-                  child: Text('MP', style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w800)),
-                ),
-                trailing: Icon(Icons.edit, color: Colors.white),
-                ),
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          SizedBox(height: 10),
+          Card(
+            // Rectangulo superior
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            margin: const EdgeInsets.all(8.0),
+            color: Color.fromRGBO(0, 208, 175, 1.0),
+            child: ListTile(
+              onTap: () {
+                // function of profile
+              },
+              title: Text("Miguel Palacios",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w900)),
+              subtitle: Text('Medellin',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w500)),
+              leading: CircleAvatar(
+                child: Text('MP',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w800)),
+                backgroundColor: Color.fromRGBO(255, 174, 0, 1.0),
               ),
-            // SizedBox(height: 40),
-            Card( // Submenu central
-              color: Colors.white,
-              elevation: 4.0,
-              margin: const EdgeInsets.fromLTRB(32.0, 8.0, 32.0, 16.0),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Icon(Icons.person, color: Color.fromRGBO(0, 208, 175, 100)),
+              trailing: Icon(Icons.edit, color: Colors.white),
+            ),
+          ),
+          // SizedBox(height: 40),
+          Card(
+            // Submenu central
+            color: Colors.white,
+            elevation: 4.0,
+            margin: const EdgeInsets.fromLTRB(32.0, 8.0, 32.0, 16.0),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            child: Column(
+              children: [
+                ListTile(
+                    leading: Icon(Icons.person,
+                        color: Color.fromRGBO(0, 208, 175, 1.0)),
                     title: Text('Información personal'),
                     trailing: Icon(Icons.keyboard_arrow_right),
-                    onTap: (){
+                    onTap: () {
                       Navigator.of(context).pushNamed('personal');
-                    }
-                  ),
-                  _buildDivider(),
-                  ListTile(
-                    leading: Icon(Icons.lock_outline, color: Color.fromRGBO(0, 208, 175, 100)),
+                    }),
+                _buildDivider(),
+                ListTile(
+                    leading: Icon(Icons.lock,
+                        color: Color.fromRGBO(0, 208, 175, 1.0)),
                     title: Text('Seguridad'),
                     trailing: Icon(Icons.keyboard_arrow_right),
-                    onTap: (){
+                    onTap: () {
                       Navigator.of(context).pushNamed('security');
-                    }
-                  ),
-                  _buildDivider(),
-                  ListTile(
-                    leading: Icon(Icons.settings, color: Color.fromRGBO(0, 208, 175, 100)),
+                    }),
+                _buildDivider(),
+                ListTile(
+                    leading: Icon(Icons.settings,
+                        color: Color.fromRGBO(0, 208, 175, 1.0)),
                     title: Text('Configuración'),
                     trailing: Icon(Icons.keyboard_arrow_right),
-                    onTap: (){
+                    onTap: () {
                       Navigator.of(context).pushNamed('settings');
-                    }
-                  ),
-                  _buildDivider(),
-                  ListTile(
-                    leading: Icon(Icons.description, color: Color.fromRGBO(0, 208, 175, 100)),
+                    }),
+                _buildDivider(),
+                ListTile(
+                    leading: Icon(Icons.help,
+                        color: Color.fromRGBO(0, 208, 175, 1.0)),
                     title: Text('Ayuda'),
                     trailing: Icon(Icons.keyboard_arrow_right),
-                    onTap: (){
+                    onTap: () {
                       Navigator.of(context).pushNamed('help');
-                    }
-                  )
-                ],
+                    })
+              ],
+            ),
+          ),
+          // SizedBox(height: 20),
+          RaisedButton(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 17.0),
+              child: Text(
+                'Cerrar Sesión',
+                style: TextStyle(fontSize: 18.0),
               ),
             ),
-            // SizedBox(height: 20),
-            FlatButton( // botón de logout
-              textColor: Colors.white,
-              onPressed: () {
-                //direcciona
-                Navigator.of(context).pushNamed('login');
-              },
-              child: Text('Cerrar Sesión', style: TextStyle(fontSize: 18.0)),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-              padding: const EdgeInsets.all(15),
-              color: Color.fromRGBO(0, 208, 175, 100)
-            ),
-          ],
-        ),
-      bottomNavigationBar: BottomNavigationBar( // menú inferior
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('', style: TextStyle(fontSize: 0),)
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.folder),
-            title: Text('', style: TextStyle(fontSize: 0),)
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('', style: TextStyle(fontSize: 0),)
+            onPressed: () {
+              //
+              Navigator.of(context).pushNamed('login');
+            },
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            elevation: 0.0,
+            color: Color.fromRGBO(0, 208, 175, 1.0),
+            textColor: Colors.white,
           ),
         ],
-        backgroundColor: Colors.grey.shade400,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // menú inferior
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text(
+                '',
+                style: TextStyle(fontSize: 0),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.folder),
+              title: Text(
+                '',
+                style: TextStyle(fontSize: 0),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text(
+                '',
+                style: TextStyle(fontSize: 0),
+              )),
+        ],
+        backgroundColor: Colors.white,
         iconSize: 40.0,
-        selectedItemColor: Color.fromRGBO(0, 208, 175, 100),
+        selectedItemColor: Color.fromRGBO(0, 208, 175, 1.0),
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
       ),
@@ -135,7 +149,8 @@ class _PerfilState extends State<Perfil> {
   }
 }
 
-Container _buildDivider() { // Raya divisoria entre opciones
+Container _buildDivider() {
+  // Raya divisoria entre opciones
   return Container(
     margin: const EdgeInsets.symmetric(horizontal: 8.0),
     width: double.infinity,
