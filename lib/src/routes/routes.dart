24daf81e2f8e71580ce57c29d09e46127483b5d4
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-10-19 18:28:39 -0500

    slider in new_property and pages of profile finished

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:login_bloc_pattern/src/pages/login_page.dart';
+import 'package:login_bloc_pattern/src/pages/homa_page.dart';
+import 'package:login_bloc_pattern/src/pages/register_page.dart';
+import 'package:login_bloc_pattern/src/pages/profile.dart';
+import 'package:login_bloc_pattern/src/pages/new_property.dart';
+import 'package:login_bloc_pattern/src/pages/personal_info.dart';
+import 'package:login_bloc_pattern/src/pages/security.dart';
+import 'package:login_bloc_pattern/src/pages/settings.dart';
+import 'package:login_bloc_pattern/src/pages/get_help.dart';
+// import 'package:login_bloc_pattern/src/pages/new_property(2).dart';
+
+Map<String, WidgetBuilder> getAppRoutes() {
+  return <String, WidgetBuilder>{
+    'login'   : (BuildContext context) => LoginPage(),
+    'home'    : (BuildContext context) => HomePage(),
+    'register': (BuildContext context) => RegisterPage(),
+    'profile' : ( BuildContext context ) => Perfil(),
+    'property': ( BuildContext context ) => PropertyPage(),
+    'personal': ( BuildContext context ) => PersonalPage(),
+    'security': ( BuildContext context ) => SecurityPage(),
+    'settings': ( BuildContext context ) => SettingsPage(),
+    'help': ( BuildContext context ) => HelpPage(),
+    // 'property2': ( BuildContext context ) => PropertyPage2(),
+  };
+}
